8cc6ee76704bff701ef8535850d8a2307d88a656
Alvin Tan <alvin.tan1912@gmail.com>
2023-05-26 21:02:12 +0800

    change postgres config

--- supabase/functions/alfred_bot/db.ts
+++ supabase/functions/alfred_bot/db.ts
@@ -1,7 +1,14 @@
+import { ClientOptions } from "https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts";
 import * as postgres from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
-const databaseUrl = Deno.env.get("SUPABASE_DB_URL") ?? "";
-const pool = new postgres.Pool(databaseUrl, 3, true);
+const clientOptions: ClientOptions = {
+  hostname: Deno.env.get("SUPABASE_DB_URL"),
+  port: 6543,
+  database: "postgres",
+  user: "postgres",
+  password: Deno.env.get("DB_PASSWORD"),
+};
+const pool = new postgres.Pool(clientOptions, 3, true);
 const db = await pool.connect();
 
 export default db;
