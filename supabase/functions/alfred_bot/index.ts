8aa0ccfff3e056169a53a466c8cbcad36d3aa20f
Alvin Tan <alvin.tan1912@gmail.com>
2023-12-08 21:28:49 +0800

    added debug logs

--- supabase/functions/alfred_bot/index.ts
+++ supabase/functions/alfred_bot/index.ts
@@ -38,91 +38,115 @@ bot.use(createConversation(SecretsService.setVoucherLink));
 
 // Basic commands
 bot.hears(/\balfred\b/i, async (ctx) => {
+  console.debug(ctx);
   await ChatsService.replyName(ctx);
 });
 
 bot.command("start", async (ctx) => {
+  console.debug(ctx);
   await ChatsService.startChat(ctx);
 });
 
-bot.command("hello", async (ctx) => await ChatsService.replyHello(ctx));
+bot.command("hello", async (ctx) => {
+  console.debug(ctx);
+  await ChatsService.replyHello(ctx);
+});
 
 // Handle the /getdinner command
 bot.command("getdinner", async (ctx) => {
+  console.debug(ctx);
   await DinnersService.getDinner(ctx);
 });
 bot.callbackQuery("get-dinner-callback", async (ctx) => {
+  console.debug(ctx);
   await DinnersService.getDinner(ctx);
 });
 
 // Handle the /startdinner command
 bot.command("startdinner", async (ctx) => {
+  console.debug(ctx);
   await DinnersService.startDinner(ctx);
 });
 bot.callbackQuery("start-dinner-callback", async (ctx) => {
+  console.debug(ctx);
   await DinnersService.startDinner(ctx);
 });
 
 // Handle the /joindinner command
 bot.command("joindinner", async (ctx) => {
+  console.debug(ctx);
   await DinnersService.joinDinner(ctx);
 });
 bot.callbackQuery("join-dinner-callback", async (ctx) => {
+  console.debug(ctx);
   await DinnersService.joinDinner(ctx);
 });
 
 // Handle the /leavedinner command
 bot.command("leavedinner", async (ctx) => {
+  console.debug(ctx);
   await DinnersService.leaveDinner(ctx);
 });
 bot.callbackQuery("leave-dinner-callback", async (ctx) => {
+  console.debug(ctx);
   await DinnersService.leaveDinner(ctx);
 });
 
 // Handle the /enddinner command
 bot.command("enddinner", async (ctx) => {
+  console.debug(ctx);
   await DinnersService.endDinner(ctx);
 });
 
 // Handle the /getwifipassword command
 bot.command("getwifipassword", async (ctx) => {
+  console.debug(ctx);
   await SecretsService.getWIFIPassword(ctx);
 });
 bot.callbackQuery("get-wifi-password-callback", async (ctx) => {
+  console.debug(ctx);
   await SecretsService.getWIFIPassword(ctx);
 });
 
 // Handle the /setwifipassword command
 bot.command("setwifipassword", async (ctx) => {
+  console.debug(ctx);
   await ctx.conversation.enter("setWIFIPassword");
 });
 bot.callbackQuery("set-wifi-password-callback", async (ctx) => {
+  console.debug(ctx);
   await ctx.conversation.enter("setWIFIPassword");
 });
 
 // Handle the /removewifipassword command
 bot.command("removewifipassword", async (ctx) => {
+  console.debug(ctx);
   await SecretsService.removeWIFIPassword(ctx);
 });
 
 // Handle the /getcdcvouchers command
 bot.command("getcdcvouchers", async (ctx) => {
+  console.debug(ctx);
   await SecretsService.getVoucherLink(ctx);
 });
 bot.callbackQuery("get-voucher-link-callback", async (ctx) => {
+  console.debug(ctx);
   await SecretsService.getVoucherLink(ctx);
 });
 
 // Handle the /setcdcvoucherlink command
 bot.command("setcdcvoucherlink", async (ctx) => {
+  console.debug(ctx);
   await ctx.conversation.enter("setVoucherLink");
 });
 bot.callbackQuery("set-voucher-link-callback", async (ctx) => {
+  console.debug(ctx);
   await ctx.conversation.enter("setVoucherLink");
 });
 
 // Handle the /removecdcvoucherlink command
 bot.command("removecdcvoucherlink", async (ctx) => {
+  console.debug(ctx);
   await SecretsService.removeVoucherLink(ctx);
 });
 
