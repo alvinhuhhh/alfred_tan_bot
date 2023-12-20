4a0786da03aaf503ee52bb2f285589075f7b2b48
Alvin Tan <alvin.tan1912@gmail.com>
2023-12-20 11:14:42 +0800

    bugfixing

--- supabase/functions/alfred_bot/repository/dinners.repository.ts
+++ supabase/functions/alfred_bot/repository/dinners.repository.ts
@@ -1,5 +1,6 @@
 import db from "./db.repository.ts";
 import Config from "../config.ts";
+import { PostgrestSingleResponse } from "https://esm.sh/v124/@supabase/postgrest-js@1.7.0/dist/module/index.js";
 
 export default class DinnersRepository {
   public static async getDinnerByDate(
@@ -8,13 +9,13 @@ export default class DinnersRepository {
   ): Promise<Dinner | undefined> {
     const ISODate: string = date.toISOString().split("T")[0];
 
-    const query = await db
+    const query: PostgrestSingleResponse<{ [x: string]: any }[]> = await db
       .from(Config.DINNER_TABLENAME)
       .select()
       .eq("chatId", chatId)
       .eq("date", ISODate);
     if (query.error) throw query.error;
-    console.log(`[getDinnerByDate] ${query}`);
+    console.log(`[getDinnerByDate] ${JSON.stringify(query)}`);
 
     if (!query.data) {
       console.log(`[getDinnerByDate] no query data`);
